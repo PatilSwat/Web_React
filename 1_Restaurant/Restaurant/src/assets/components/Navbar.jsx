86df4bf5fa292e8ef9f849d02669416f0fa2ecfc
PatilSwat <swatipatil8248@gmail.com>
2024-06-15 01:05:43 +0530

    changes added

--- 1_Restaurant/Restaurant/src/assets/components/Navbar.jsx
+++ 1_Restaurant/Restaurant/src/assets/components/Navbar.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import { PiCaretDown } from "react-icons/pi";
-import { IoSearch } from "react-icons/io5";
+import { IoIosSearch } from "react-icons/io";
+import { BiSolidOffer } from "react-icons/bi";
+import { IoHelpBuoyOutline } from "react-icons/io5";
+import { VscAccount } from "react-icons/vsc";
+import { BsCartCheck } from "react-icons/bs";
 
 const Navbar = () => {
   const [toggle , setToggle] = useState(false);
@@ -9,16 +13,53 @@ const Navbar = () => {
     setToggle(true);
  
   }
+  const hideSlideShow = () => {
+    setToggle(false);
+  }
+
+  const links = [
+    {
+      icon :"" ,
+      name :"Swiggy Corporate"
+    },
+    {
+      icon : <IoIosSearch/>,
+      name :"Search"
+    },
+    {
+      icon : <BiSolidOffer />,
+      name :"Offer",
+      sup :"new"
+    },
+    {
+      icon : <IoHelpBuoyOutline />,
+      name :"Help"
+    },
+    {
+      icon : <VscAccount />,
+      name :"Sign in"
+    },
+    {
+      icon : <BsCartCheck />,
+      name :"Cart"
+    }
+  ]
 
   return (
    <>
 
     <div>
-    <div className='black-overlay w-full h-full fixed duration-500 ' style={{
+    <div className='black-overlay w-full h-full fixed duration-500 ' onClick= {hideSlideShow} style={{
       opacity : toggle ? 1 : 0 ,
       visibility : toggle ? "visible" : "hidden"
   
-    }} ></div>
+    }} >
+      <div  onClick={(e) =>{
+        e.stopPropagation();
+      }} className='w-[400px] h-full bg-white absolute duration-[400ms] ' style={{
+        left : toggle ? '0%' : '-100%'
+      }}> hii</div>
+    </div>
         <navbar>
         <div className='p-[15px] shadow-xl'>
           <div className='max-w-[1200px] mx-auto border border-red-500 flex items-center'>
@@ -27,9 +68,23 @@ const Navbar = () => {
 
               </div>
               <div > Swati <span className='font-semibold underline gap-2 text-gray-700'> Other</span> <PiCaretDown onClick={showSideMenu} fontSize={25} className='inline gap-2 text-[0.9rem] text-[#fc8019] cursor-pointer'/></div>
-              <div>
-                <div className='font-semibold inline' >Search<IoSearch /></div>
-              </div>
+              <nav className='flex list-none gap-10 ml-auto p-3 font-normal text-[18px] '>
+              {
+                links.map(
+                  (link, index) => {
+                    return <li className='flex cursor-pointer hover:text-[#fc8019] items-center gap-2'>
+                              {link.icon}
+                               {link.name}
+                               <sup>{link.sup}</sup>
+                               </li>
+               
+                  }
+                )
+              }
+
+               
+             
+              </nav>
           </div>
         </div>
         </navbar>
